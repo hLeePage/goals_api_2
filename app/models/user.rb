01ac33c06298fdd39871cba93550ae499590c907
hLeePage <hLeepage@yahoo.com>
2015-12-14 15:58:31 -0600

    Make minor formatting changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+# Table associaions
   has_many :goals, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",
                                 foreign_key: "follower_id",
                                 dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
 
-  validates :password, length: { minimum: 8 }
-
-
+# Validations
   before_save { self.email = email.downcase }
   before_save { self.username = username.downcase }
-
+  validates :password, length: { minimum: 8 }
   validates :first, presence: true, length: { maximum: 10 }
   validates :last, presence: true, length: { maximum: 20 }
   validates :username, presence: true, length: { maximum: 50 },
@@ -22,22 +21,22 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-# Follows a user.
+# Follow a user.
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
   end
 
-  # Unfollows a user.
+# Unfollow a user.
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
-  # Returns true if the current user is following the other user.
+# Return true if the current user is following the other user.
   def following?(other_user)
     following.include?(other_user)
   end
 
-
+# Required for Doorkeeper
   def self.authenticate!(email, password)
     user = User.find_by_email(email)
     return false unless user
