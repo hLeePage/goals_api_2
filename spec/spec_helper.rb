1c61e51a260265988ba9b16ba5bc31d06c396456
hLeePage <hLeepage@yahoo.com>
2015-12-14 10:54:53 -0600

    Tweak rails_helper and spec_helper to use Database Cleaner and Factory Girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,16 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.order = :random
+
+  config.before(:suite) do
+  begin
+    DatabaseCleaner.start
+    FactoryGirl.lint
+  ensure
+    DatabaseCleaner.clean
+  end
+  end
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
