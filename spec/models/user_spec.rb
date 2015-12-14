acd56fcdd8d2897e1c46a968db87d9b0d21ae1f8
hLeePage <hLeepage@yahoo.com>
2015-12-14 16:24:18 -0600

    Add tests for User model #follow, #unfollow

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Users" do
+
+  let(:user1) { FactoryGirl.create :user }
+  let(:user2) { FactoryGirl.create :user }
+
+  describe "#follow, #unfollow, #following?" do
+    it "enables users to follow other users" do
+      expect(user1.following?(user2)).to be false
+      user1.follow(user2)
+      expect(user1.following?(user2)).to be true
+      user1.unfollow(user2)
+      expect(user1.following?(user2)).to be false
+    end
+  end
+
+end
