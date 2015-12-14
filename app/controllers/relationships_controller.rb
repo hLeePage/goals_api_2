7b0797d12cf767cee9399217fafeb59c0d11fdf1
hLeePage <hLeepage@yahoo.com>
2015-12-14 16:56:53 -0600

    Add #create and #destroy to Relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,13 @@
 class RelationshipsController < ApplicationController
+
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+  end
+
+  def destroy
+    user = Relationship.find(params[:id])
+    current_user.unfollow(user)
+  end
+
 end
