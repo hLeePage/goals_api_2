caa6a62069cea78ee170e51d864b7bdd4a0c08b6
hLeePage <hLeepage@yahoo.com>
2015-12-14 16:32:01 -0600

    Add tests for user.followers.include?(other_follower) to User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe "Users" do
   let(:user2) { FactoryGirl.create :user }
 
   describe "#follow, #unfollow, #following?" do
-    it "enables users to follow other users" do
+    it "enables users to follow/unfollow other users" do
       expect(user1.following?(user2)).to be false
       user1.follow(user2)
       expect(user1.following?(user2)).to be true
+      expect(user2.followers.include?(user1)).to be true
       user1.unfollow(user2)
       expect(user1.following?(user2)).to be false
+      expect(user2.followers.include?(user1)).to be false
     end
   end
 
