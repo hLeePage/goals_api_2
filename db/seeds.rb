38f103bda14983559fb481a027f68e1940e92d26
hLeePage <hLeepage@yahoo.com>
2015-12-14 17:40:33 -0600

    Add three users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(first: 'Kermit',
+            last: 'Frog',
+            username: 'Kermit',
+            email: 'kermit@frog.com',
+            password: 'password')
+
+User.create(first: 'Miss',
+            last: 'Piggy',
+            username: 'Miss',
+            email: 'miss@piggy.com',
+            password: 'password')
+
+User.create(first: "Fozzy",
+            last: "Bear",
+            username: "Fozzy",
+            email: "fozzy@bear.com",
+            password: 'password')
