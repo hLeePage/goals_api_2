f655c8b746e4914c3d3d56e6eb4b11d0ffc339dd
hLeePage <hLeepage@yahoo.com>
2015-12-14 16:26:51 -0600

    Add has_many passive relationships to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ActiveRecord::Base
 # Table associaions
   has_many :goals, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",
-                                foreign_key: "follower_id",
-                                dependent:   :destroy
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
-
+  has_many :followers, through: :passive_relationships
+  
 # Validations
   before_save { self.email = email.downcase }
   before_save { self.username = username.downcase }
