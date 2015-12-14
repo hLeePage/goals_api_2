3c39b0b779cc14117305d94d5f3291710068a2fc
hLeePage <hLeepage@yahoo.com>
2015-12-14 15:49:26 -0600

    Add model tests for Users and Relationship

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "Relationships" do
+
+  describe "#create" do
+    let(:relationship) { Relationship.create(follower_id: 1, followed_id: 2) }
+
+    it "creates a new relationship" do
+      #pry.bind
+      expect(relationship[:follower_id]).to eq(1)
+    end
+
+    it "should be valid" do
+      expect(relationship).to be_valid
+    end
+
+    it "should require a follower_id" do
+      relationship[:follower_id] = nil
+      expect(relationship).to_not be_valid
+    end
+
+    it "should require a followed_id" do
+      relationship[:followed_id] = nil
+      expect(relationship).to_not be_valid
+    end
+
+  end
+
 end
