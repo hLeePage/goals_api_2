ccaf888f9d2280ff47ad6507361574ea856aa58d
hLeePage <hLeepage@yahoo.com>
2015-12-14 17:04:10 -0600

    Add following, followers (GET) to users resource. Also add relationships resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   #use_doorkeeper
 
-  resources :goals, except: [:new, :edit]
-  resources :users, except: [:new, :edit]
+  resources :users,               except: [:new, :edit] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :goals,               except: [:new, :edit]
+  resources :relationships,       only: [:create, :destroy]
 end
