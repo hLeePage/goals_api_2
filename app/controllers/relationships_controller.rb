fd076a307dadd308e14c8f6c103f7ba4aacd0a24
hLeePage <hLeepage@yahoo.com>
2015-12-15 10:50:58 -0600

    Small formatting change

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,6 +3,10 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
+
+    # {
+    #   followed_id: 4567
+    # }
   end
 
   def destroy
