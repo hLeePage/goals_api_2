7a5adc431be7c09a877d372578d42d881d15518f
hLeePage <hLeepage@yahoo.com>
2015-12-16 10:23:49 -0600

    Delete request specs for Goals

--- spec/requests/goals_spec.rb
+++ spec/requests/goals_spec.rb
@@ -6,33 +6,6 @@ RSpec.describe "Goals" do
   let(:user1) { FactoryGirl.create :user }
   let(:user2) { FactoryGirl.create :user }
   let(:user3) { FactoryGirl.create :user }
-  let(:token) { FactoryGirl.create(:access_token, resource_owner_id: user.id) }
-
-
-  describe "#create" do
-    let(:payload) { { goal: FactoryGirl.attributes_for(:goal) } }
-
-    it "successfully creates a user whose parameters are valid" do
-      post goals_path, payload
-      expect(response).to have_http_status(:created)
-    end
-
-    it "returns an error when username is empty" do
-      payload[:goal][:body] = ""
-      post goals_path, payload
-      expect(response).to have_http_status(422)
-    end
-
-    it "correctly associates goal.user_id with user.id" do
-      user1
-      user2
-      payload[:goal][:user_id] = user2.id
-      #pry.bind
-      post goals_path, payload
-      #pry.bind
-      expect(json["user_id"]).to eq(user2.id)
-    end
-
-  end
+  let(:token) { FactoryGirl.create(:access_token, resource_owner_id: user1.id) }
 
 end
