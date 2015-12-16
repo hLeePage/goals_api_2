e199c45f9240cebec71622a00ecaaf6e9d23ec8c
hLeePage <hLeepage@yahoo.com>
2015-12-15 22:30:58 -0600

    Correct typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :goals,               except: [:new, :edit]
-  
+  resources :goals,               except: [:new, :edit] do
+    resources :comments,           except: [:new, :edit]
+  end
+
   post "/follow/:user_id" => "relationships#follow", as: :follow
   post "/unfollow/:user_id" => "relationships#unfollow", as: :unfollow
 
