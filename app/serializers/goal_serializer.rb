882e2dd85c566930b5c0ad5f36d1c418a9fc2619
hLeePage <hLeepage@yahoo.com>
2015-12-17 17:47:58 -0600

    Add :upvotes, :downvotes, :comments to GoalSerializer

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,3 +1,18 @@
 class GoalSerializer < ActiveModel::Serializer
-  attributes :id, :body, :user_id, :completed
+  attributes :id, :user_id, :username, :body, :upvotes, :downvotes, :completed, :comments
+
+  has_many :comments
+
+  def username
+    user = User.find(user_id)
+    user.username
+  end
+
+  def upvotes
+    object.upvotes
+  end
+
+  def downvotes
+    object.downvotes
+  end
 end
