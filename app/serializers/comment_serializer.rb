cf37546cc33886813698ab7d6a74cf9209fd37b5
hLeePage <hLeepage@yahoo.com>
2015-12-17 17:42:06 -0600

    Exchange :username for :user_id

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :goal_id, :body, :created_at
+  attributes :id, :username, :user_id, :goal_id, :body, :created_at
+
+  def username
+    user = User.find(user_id)
+    user.username
+  end
+
 end
